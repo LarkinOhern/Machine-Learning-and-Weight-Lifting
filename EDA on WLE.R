fbff28a2b11d52be8f283820f23c408489a8f11e
LarkinOhern <larkin.ohern.85@gmail.com>
2016-08-08 21:29:17 -0400

    more eda

--- EDA on WLE.R
+++ EDA on WLE.R
@@ -9,7 +9,7 @@ trainingdata<-read.csv("trainingdata.csv", header = TRUE)
 
 ###and now the test data but never look at it...
 url1<-"https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv"
-download.file(url, "testdata.csv")
+download.file(url1, "testdata.csv")
 
 testdata<-read.csv("testdata.csv", header=TRUE)
 
